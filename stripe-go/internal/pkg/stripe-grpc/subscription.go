02d2157f270f155028fa1ef82ffac91dda700ba1
lee-aaron <lee.aaron.68@gmail.com>
2022-07-26 11:18:34 -0700

    Changed fees back to 10%

--- stripe-go/internal/pkg/stripe-grpc/subscription.go
+++ stripe-go/internal/pkg/stripe-grpc/subscription.go
@@ -26,7 +26,7 @@ func (srv *SubscriptionServer) CreateSubscription(ctx context.Context, req *pb.C
 			},
 		},
 		PaymentBehavior:       stripe.String("default_incomplete"),
-		ApplicationFeePercent: stripe.Float64(5),
+		ApplicationFeePercent: stripe.Float64(10),
 	}
 
 	params.AddExpand("latest_invoice.payment_intent")
