843734bae6d62a33383f1b151cde32d4692bdbbf
lee-aaron <lee.aaron.68@gmail.com>
2022-07-26 11:30:05 -0700

    Added google analytics

--- scythe/pages/_document.tsx
+++ scythe/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -19,6 +20,19 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap"
           rel="stylesheet"
         />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-868074880"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-868074880');
+            gtag('event', 'conversion', {'send_to': 'AW-868074880/ITOFCOmQ0s8DEICL950D'});
+            `}
+        </Script>
       </Head>
       <body>
         <Main />
